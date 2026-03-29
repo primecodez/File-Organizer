a44009b4e3778937cca40f0fc55da341ce070251
primecodez <narendrakumarsingh2859@gmail.com>
2026-03-29 11:44:47 +0000

    Initial project setup with skeleton functions

---
+++ organizer.py
@@ -0,0 +1,14 @@
+import os
+import shutil
+
+def get_file_type(filename):
+    pass
+
+def create_folder_if_not_exists(folder_name):
+    pass
+
+def move_file(file, destination):
+    pass
+
+if __name__ == "__main__":
+    print("File Organizer starting...")
